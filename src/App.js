1186e306ee1abebcc53ab8a085b55dfc8c531123
Julian Yates <julianyates@julians-mbp.attlocal.net>
2019-08-07 21:39:56 -0500

    MVP DONE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {BrowserRouter as Route} from "react-router-dom";
 
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation> 
+    </Navigation>
+    <Route exact path="/" />
+    <Route path="/about" />
+    <Route path="/contact" />
   </div>
 );
 
